c176d4736c837113e7e02f3b3beb217b9f2380e1
Yasin Edin <yasin.edin@outlook.de>
2020-12-02 17:01:21 +0100

    implemented generalisation

---
+++ autoML - Yasin/utils.py
@@ -0,0 +1,78 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+
+aws_user = os.environ['AWS_USER']
+aws_key = os.environ['AWS_KEY']
+region_name = 'eu-central-1'
+
+session = boto3.session.Session(
+    aws_access_key_id=aws_user,
+    aws_secret_access_key=aws_key,
+    region_name=region_name)
+
+s3_client = session.client('s3')
+s3 = session.resource('s3')
+
+month_year = 'string'
+
+# --------------------------------------------------------------------------
+def get_preproccessed_df(s3_link):
+    df_raw = pd.read_csv(s3_link, dtype={'DAY_OF_MONTH': str, 'CRS_ARR_TIME': str, 'CRS_DEP_TIME': str, 'WHEELS_OFF': str})
+    columns_to_drop = ['YEAR', 'MONTH', 'FL_DATE', 'ORIGIN_STATE_NM', 'DEST_CITY_NAME', 'DEST_STATE_NM', 'DEP_TIME', 
+                       'DEP_TIME_BLK', 'ARR_TIME', 'ARR_TIME_BLK', 'CARRIER_DELAY', 'WEATHER_DELAY', 'NAS_DELAY', 
+                       'SECURITY_DELAY', 'LATE_AIRCRAFT_DELAY', 'FLIGHTS', 'Unnamed: 54', 
+                       'OP_CARRIER_AIRLINE_ID',	'OP_CARRIER_FL_NUM', 'ORIGIN_AIRPORT_ID', 'ORIGIN_AIRPORT_SEQ_ID',	
+                       'ORIGIN_CITY_MARKET_ID',	'ORIGIN_STATE_FIPS', 'DEST_AIRPORT_ID',	'DEST_AIRPORT_SEQ_ID',	
+                       'DEST_CITY_MARKET_ID', 'DEST_STATE_FIPS', 'DEP_DELAY', 'DEP_DELAY_NEW', 'DEP_DEL15', 'DEP_DELAY_GROUP', 
+                       'WHEELS_ON', 'ARR_DELAY', 'ARR_DEL15', 'ARR_DELAY_GROUP', 'CRS_ELAPSED_TIME', 'ACTUAL_ELAPSED_TIME', 
+                       'AIR_TIME', 'OP_CARRIER',
+                       'TAIL_NUM', 'ORIGIN_CITY_NAME']
+    
+    # drop flights which are either canceled or diverted
+    # as this is a conceptually different thing than being delayed
+    df_raw = df_raw.drop(df_raw.index[(df_raw['CANCELLED'] == 1) | 
+                         (df_raw['DIVERTED'] == 1)]).reset_index(drop=True)
+    
+    df_raw = df_raw.drop(columns_to_drop, axis=1)
+    df_raw = df_raw.drop(['CANCELLED', 'DIVERTED'], axis=1)
+    
+    # to get label column up front
+    df_raw = df_raw[sorted(df_raw.columns)]
+    
+    return df_raw
+
+
+# --------------------------------------------------------------------------
+def split_and_upload_df(df):
+    # split data
+    df_train, df_test = train_test_split(df, 
+                                     test_size=0.2,
+                                     random_state=42)
+    
+    file_path_train = f'/Users/yasinedin/Downloads/automl/{month_year}train.csv'
+    file_path_test = f'/Users/yasinedin/Downloads/automl/{month_year}test.csv'
+    
+    df_train.to_csv(file_path_train, index=False, header=True)
+    df_test.drop('ARR_DELAY_NEW', axis=1).to_csv(file_path_test, index=False, header=False)
+    
+    # upload data to s3
+    try:
+        train_response = s3_client.upload_file(file_path_train, bucket_name, f'automl/{month_year}_train.csv')
+        print('Train data successfully uploaded')
+    except ClientError as e:
+        logging.error(e)
+        print('Something went wrong.')
+
+    try:
+        test_response = s3_client.upload_file(file_path_test, bucket_name, f'automl/{month_year}_test.csv')
+        print('Test data successfully uploaded')
+    except ClientError as e:
+        logging.error(e)
+        print('Something went wrong.')
+        
+    return print('Successfully split and uploaded data')
